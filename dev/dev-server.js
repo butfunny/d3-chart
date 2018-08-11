575ce6851d2e0e0ad5fe937f68adfca7caa051c3
CuongNguyen <p1code@prototype1.io>
2018-08-11 08:25:44 +0700

    init

--- dev/dev-server.js
+++ dev/dev-server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
-
 const server = require('http').Server(app);
+
+app.use(express.static(__dirname));
 
 app.get("*", (req, res) => {
     res.sendFile(__dirname + "/index.html");
 });
+
 
 const port = process.env.PORT || 3000;
 server.listen(port, () => {
